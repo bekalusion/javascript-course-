bc925447fd86d20cc6aa45e0e1d28ce05226cc7b
bekalusion <121852629+bekalusion@users.noreply.github.com>
2023-01-09 13:02:13 +0400

    Create assignmentValuesandVariables.js

---
+++ assignmentValuesandVariables.js
@@ -0,0 +1,11 @@
+let country = 'georgia';
+let continent = 'europe';
+let population = 4000000;
+
+console.log(country)
+
+console.log(continent)
+console.log(population)
+
+country = 'yes'
+console.log(country);
